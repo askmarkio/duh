askmarkio/duh

commit ae00daeb44d0096b1f3cf75c5480cd9f65c52276

added support to read and write objects

libwyag.py
@@ -156,3 +156,174 @@ def cmd_init(args):
     assert isinstance(args.path, str), "The 'path' argument must be a string."
     repo_create(args.path)
 
+def repo_find(path=".", required=True):
+    path = os.path.realpath(path)
+
+    if os.path.isdir(os.path.join(path, ".git")):
+        return GitRepository(path)
+
+    # If we haven't returned, recurse in parent, if w
+    parent = os.path.realpath(os.path.join(path, ".."))
+
+    if parent == path:
+    # Bottom case
+    # os.path.join("/", "..") == "/":
+    # If parent==path, then path is root.
+        if required:
+            raise Excetion("No git directory.")
+        else:
+            return None
+
+    # Recursive case
+    return repo_find(parent, required)
+
+class GitObject (object):
+
+    def __init__(self, data=None):
+        if data != None:
+            self.deserialize(data)
+        else:
+            self.init()
+
+    def serialize(self, repo):
+        """This function MUST be implemented by subclasses.
+        It must read the object's contents from self.data, a byte string, and do 
+        whatever it takes to convert it into a meaningful representation.
+        What exactly that means depend on each subclass."""
+        raise Exception("Unimplemented!")
+
+    def deserialize(self, data):
+        raise Exception("Unimplemented")
+
+    def init(self):
+        pass # Just do nothing. This is a reasonable default!
+
+def object_read(repo, sha):
+    """Read object sha from Git repository repo. Return a 
+    GitObhect whose exavt type depends on the object.""""
+
+    path = repo_find(repo, "objects", sha[0:2], sha[2:])
+
+    if not os.path.isfile(path):
+        return None
+    
+    with open (path, "rb") as f:
+        raw = zlib.decompress(f.read())
+
+        # Read object type
+        x = raw.find(b' ')
+        fmt = raw[0:x]
+
+        #Read and validate object serialize
+        y = raw.find(b'\x00', x)
+        size = int(raw[x:y].decode("ascii"))
+    if size != len(raw)-y-1:
+        raise Exception("Malformed obhect {0}: bad length".format(sha))
+
+    # Pick constructor
+    match fmt:
+        case b'commit'    : c=GitCommit
+        case b'tree'      : c=GitTree
+        case b'tag'       : c=GitTag
+        case b'blob'      : c=GitBlob
+        case _:
+            raise Exception("Unknown type {0} for object {1}".format(fmt.decode("ascii"), sha))
+
+    # Call constructor and return object
+    return c(raw[y+1:])
+
+def object_write(obj, repo=None):
+    # Serialize object data
+    data = obj.serialize()
+    # Add header
+    result = obj.fmt + b' ' + str(len(data)).encode() + b'\x00' + data
+    # Compute hash
+    sha = hashlib.sha1(result).hexdigest()
+
+    if repo:
+    # Compute path
+        path=repo_file(repo, "objects", sha[0:2], sha[2:], mkdir=True)
+
+        if not os.path.exists(path):
+            with open(path, 'wb') as f:
+                # Compress and write
+                f.write(zlib.compress(result))
+
+    return sha
+
+class GitBlob(GitObject):
+    fmt=b'blob'
+
+    def serialize(self):
+        return self.blobdata
+
+    def deserialize(self, data):
+        self.blobdata = data
+
+argsp = argsubparsers.add_parser("cat-file",
+                                 help="Provide content of repository objects")
+
+argsp.add_argument("type",
+                   metavar="type",
+                   choices=["blob", "commit", "tag", "tree"],
+                   help="Specify the type")
+
+argsp.add_argument("object",
+                   metavar="object",
+                   help="The objects to display")
+
+def cmd_cat_file(args):
+    repo = repo_find()
+    cat_file(repo, args.object, fmt=args.type.encode())
+
+def cat_file(repo, obj, fmt=None):
+    obj = object_read(repo, object_find(repo, obj, fmt=fmt))
+    sys.stdout.buffer.write(obj.serialize())
+
+def object_find(repo, name, fmt=None, follow=True):
+    return name
+
+argsp = argsubparsers.add_parser(
+    "hash-object",
+    help="Computer obhect ID and optionally creates a blob from a file")
+
+argsp.add_argument("-t",
+                   metavar="type",
+                   dest="type",
+                   choices=["blob", "commit", "tag", "tree"],
+                   default="blob",
+                   help="Specifythe type")
+
+argsp,add_argument("-w",
+                   dest="write",
+                   action="store_true",
+                   help="Actually write the object into the database")
+
+argsp.add_argument("path",
+                   help="Read object from <file>")
+
+def cmd_hash_object(args):
+    if args.write:
+        repo = repo_find()
+    else:
+        repo = None
+
+    with open(args.path, "rb") as fd:
+        sha = object_hash(fd, args.type.encode(), repo)
+        print(sha)
+
+def object_hash(fd, fmt, repo=None):
+    """ Hash object, writing it to repo if provided."""
+    data = fd.read()
+
+    # Choose constructor according to fmt argument
+    match fmt:
+        
+        case b'commit'    : obj=GitCommit(data)
+        case b'tree'      : obj=GitTree(data)
+        case b'tag'       : obj=GitTag(data)
+        case b'blob'      : obj=GitBlob(data)
+        case _:
+            raise Exception("Unknown type %s!" % fmt)
+
+    return object_write(obj, repo)

commit 8ca460bfe2e5f097213d8014576278e7ed585c25

#16 - Fixed typos that were breaking the arguments
The typos were introduced in commit ae00daeb for adding read and write
support for objects.

libwyag.py
@@ -199,8 +199,10 @@ class GitObject (object):
         pass # Just do nothing. This is a reasonable default!
 
 def object_read(repo, sha):
-    """Read object sha from Git repository repo. Return a 
-    GitObhect whose exavt type depends on the object.""""
+    """
+    Read object sha from Git repository repo. Return a 
+    GitObhect whose exavt type depends on the object.
+    """
 
     path = repo_find(repo, "objects", sha[0:2], sha[2:])
 
@@ -292,9 +294,9 @@ argsp.add_argument("-t",
                    dest="type",
                    choices=["blob", "commit", "tag", "tree"],
                    default="blob",
-                   help="Specifythe type")
+                   help="Specify the type")
 
-argsp,add_argument("-w",
+argsp.add_argument("-w",
                    dest="write",
                    action="store_true",
                    help="Actually write the object into the database")